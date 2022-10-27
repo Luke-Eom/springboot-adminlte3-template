f8518fbbdfaf62cbed2aea2c63b0da7c3fa33475
LukeEom <eluke1028@gmail.com>
2022-10-27 17:16:10 +0900

    #3 productRepo
    dto/entity mapping required -> crud
    / NextAction : controller to view

--- src/main/java/com/hendisantika/config/FaviconConfiguration.java
+++ src/main/java/com/hendisantika/config/FaviconConfiguration.java
@@ -1,15 +1,14 @@
 package com.hendisantika.config;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
